214259d7c85b3eba82e05ccbd01f0dd76d605800
engam <engamrdiab52@gmail.com>
2021-10-08 09:08:27 +0200

    add logic

--- app/src/main/java/com/amrabdelhamiddiab/notification/MainActivity.kt
+++ app/src/main/java/com/amrabdelhamiddiab/notification/MainActivity.kt
@@ -1,11 +1,72 @@
 package com.amrabdelhamiddiab.notification
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 
 class MainActivity : AppCompatActivity() {
+    private val CHANNEL_ID: String = "channel_id_example_01"
+    private val notificationId = 101
+    private lateinit var button: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        button = findViewById(R.id.button)
+        createNotificationChannel()
+        button.setOnClickListener {
+            sendNotification()
+        }
+    }
+
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = "Notification Channel"
+            val descriptionText = "Notification Description"
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel: NotificationChannel =
+                NotificationChannel(CHANNEL_ID, name, importance).apply {
+                    description = descriptionText
+                }
+            val notificationManager: NotificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+
+    private fun sendNotification() {
+        val intent = Intent(this, MainActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
+        val pendingIntent: PendingIntent = PendingIntent.getActivity(this, 0, intent, 0)
+        val bitmap: Bitmap = BitmapFactory.decodeResource(
+            applicationContext.resources,
+            R.drawable.ic_bag
+        )
+        val bitmapLargeIcon: Bitmap = BitmapFactory.decodeResource(
+            applicationContext.resources,
+            R.drawable.ic_bag
+        )
+
+        val builder = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setContentTitle("Example Title")
+            .setContentText("Example description")
+            .setLargeIcon(bitmapLargeIcon)
+            .setContentIntent(pendingIntent)
+            .setStyle(NotificationCompat.BigPictureStyle().bigPicture(bitmap))
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+        with(NotificationManagerCompat.from(this)) {
+            notify(notificationId, builder.build())
+        }
     }
 }
